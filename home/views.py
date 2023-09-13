b6c4810ace869d61d3b40e535fd389e583af1544
kibetamos <amos.rotich@strathmore.edu>
2023-09-13 18:27:45 +0300

    added home app

--- home/views.py
+++ home/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.http import HttpResponse 
+
+
+def credits(request): 
+    content = "Nicky\nYour Name" 
+        
+    return HttpResponse(content, content_type="text/plain")
