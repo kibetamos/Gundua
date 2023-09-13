9f7020821d219ef567a68f40f4e08ccbe3593b11
kibetamos <amos.rotich@strathmore.edu>
2023-09-13 19:49:51 +0300

    added new home mgartions

--- home/admin.py
+++ home/admin.py
@@ -1,5 +1,14 @@
 from django.contrib import admin
 from home.models import Person, Crime
 # Register your models here.
+
+
 # @admin.register(Person)
-# @admin.register(Crime)
+@admin.register(Person)
+class PersonAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Crime)
+class CrimeAdmin(admin.ModelAdmin):
+    pass
