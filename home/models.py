017c8ef86f1d81b97445e361c10d53806ed58688
kibetamos <amos.rotich@strathmore.edu>
2023-09-14 06:44:24 +0300

    updated models of the crime

--- home/models.py
+++ home/models.py
@@ -3,6 +3,9 @@ from django.db import models
 # Create your models here.
 
 class Crime(models.Model):
+    id_no = models.CharField(max_length=10, unique=True).
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
     date = models.DateField()
     time = models.TimeField()
     location = models.CharField(max_length=50)
