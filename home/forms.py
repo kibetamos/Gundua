05c5dcb7ac38c7c6f99a8e2ecc5049c5618efabd
kibetamos <amos.rotich@strathmore.edu>
2023-09-14 06:49:42 +0300

    updated forms of the crime

--- home/forms.py
+++ home/forms.py
@@ -5,8 +5,11 @@ from .models import Crime
 class CrimeForm(forms.ModelForm):
     class Meta:
         model = Crime
-        fields = ['date', 'time', 'location', 'occur', 'action', 'type']
+        fields = ['id_no', 'first_name','last_name', 'date', 'time', 'location', 'occur', 'action', 'type']
 
-
+        widgets = {
+                    'date': forms.DateInput(attrs={'type': 'date'}),
+                    'time': forms.TimeInput(attrs={'type': 'time'}),
+                }
     # Additional fields can be added here, if needed
     # attachments = forms.FileField(required=False, widget=forms.ClearableFileInput(attrs={'multiple': True}))
